befd55a648413d318f247f1aa67c2e378f67418f
Lau <laurentiucozma12@gmail.com>
2023-07-01 16:45:55 +0300

    single_post

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,10 @@ Route::get('/', function () {
     return view('home');
 });
 
+Route::get('/post', function () {
+    return view('post');
+});
+
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
