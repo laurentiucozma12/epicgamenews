b1b91cd97f6fedb171b0e7fd10386fa9fa969071
Lau <laurentiucozma12@gmail.com>
2023-07-04 21:20:25 +0300

    more migrations

---
+++ app/Models/Post.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+use App\Models\User;
+use App\Models\Category;
+
+class Post extends Model
+{
+    use HasFactory;   
+    
+    protected $fillable = ['title', 'slug', 'excerpt', 'body', 'user_id', 'category_id'];
+
+    public function author()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+}
--- app/Models/Role.php
+++ app/Models/Role.php
@@ -10,6 +10,8 @@ class Role extends Model
 {
     use HasFactory;
 
+    protected $fillable = ['name'];
+    
     public function users() 
     {
         return $this->hasMany(User::class);
