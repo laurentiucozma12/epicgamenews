70af30e70b2a8e6ffe4ac08534814c957886e4a9
Lau <laurentiucozma12@gmail.com>
2023-07-06 12:05:05 +0300

    space

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,7 @@ use Illuminate\Support\Facades\Route;
 | routes are loaded by the RouteServiceProvider and all of them will
 | be assigned to the "web" middleware group. Make something great!
 |
-*/
+*/ 
 
 use App\Models\Comment;
 use App\Models\Post;
