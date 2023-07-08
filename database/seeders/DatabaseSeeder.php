5032758b123cfc2ba10ee71e4c0103c267abf4df
Lau <laurentiucozma12@gmail.com>
2023-07-08 10:51:27 +0300

    comment factory

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -22,6 +22,7 @@ class DatabaseSeeder extends Seeder
         
         \App\Models\Category::truncate();
         \App\Models\Post::truncate();
+        \App\Models\Comment::truncate();
 
         Schema::enableForeignKeyConstraints();
 
@@ -31,5 +32,6 @@ class DatabaseSeeder extends Seeder
 
         \App\Models\User::factory(10)->create();
         \App\Models\Post::factory(100)->create();
+        \App\Models\Comment::factory(100)->create();
     }
 }
