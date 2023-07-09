a54ae47a0118a51864d3660a7550da8ac70cc7d2
Lau <laurentiucozma12@gmail.com>
2023-07-09 14:31:30 +0300

    imageFactory

--- database/factories/ImageFactory.php
+++ database/factories/ImageFactory.php
@@ -3,6 +3,7 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Random\RandomError;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Image>
@@ -16,10 +17,23 @@ class ImageFactory extends Factory
      */
     public function definition(): array
     {
+        $fake_images = [
+          '1.jpg',  
+          '2.jpg',  
+          '3.jpg',  
+          '4.jpg',  
+          '5.jpg',  
+          '6.jpg',  
+          '7.jpg',  
+          '8.jpg',  
+          '9.jpg',  
+          '10.jpg',  
+        ];
+
         return [
             'name' => $this->faker->word(),
             'extension' => 'jpg',
-            'path' => '/public/images/' . $this->faker->word() . '.' . 'jpg',
+            'path' => 'images/' . $this->faker->RandomElement($fake_images),
         ];
     }
 }
