d89ec5e99b83b7cf7d3823d32ba26a9fb97e3c9c
Lau <laurentiucozma12@gmail.com>
2023-07-10 13:09:24 +0300

    recent posts

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -55,11 +55,18 @@
 
 						@foreach($recent_posts as $recent_post)
 							<div class="f-blog">
-								<a href="blog.html" class="blog-img" style="background-image: url(blog_template/images/blog-1.jpg);">
+								<a 
+								href="blog.html" 
+								class="blog-img" 
+								style="background-image: url({{ asset( 'storage/' . $recent_post->image->path. '' ) }});">
 								</a>
 								<div class="desc">
 									<p class="admin"><span> {{ $recent_post->created_at->diffForHumans() }} </span></p>
-									<h2><a href="blog.html"> {{ $recent_post->title }} </a></h2>
+									<h2>
+										<a href="blog.html"> 
+											{{ \Str::limit($recent_post->title, 20) }}									
+										</a>
+									</h2>
 									<p> {{ $recent_post->excerpt }} </p>
 								</div>
 							</div>
