7055eb988b52f5892ce47e51e915a6045d928e8e
Lau <laurentiucozma12@gmail.com>
2023-07-13 14:31:27 +0300

    space

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -11,24 +11,24 @@
 
 				@forelse ($posts as $post)
 					<div class="block-21 d-flex animate-box post">
-					<a 
-					href="{{ route('posts.show', $post) }}" 
-					class="blog-img"
-					style="background-image: url({{ asset( 'storage/' . $post->image->path. '' ) }});"></a>
+						<a 
+						href="{{ route('posts.show', $post) }}" 
+						class="blog-img"
+						style="background-image: url({{ asset( 'storage/' . $post->image->path. '' ) }});"></a>
 
-					<div class="text">
-						<h3 class="heading"><a href="{{ route('posts.show', $post) }}"> {{ $post->title }} </a></h3>
-						<p class="excerpt"> {{ $post->excerpt }} </p>
-						<div class="meta">
-							<div><a class='date' href="#"><span class="icon-calendar"></span> {{ $post->created_at->diffForHumans() }} </a></div>
-							<div><a href="#"><span class="icon-user2"></span> {{ $post->author->name }} </a></div>
-							<div class="comments-count">
-								<a href="{{ route('posts.show', $post) }}#post-comments">
-									<span class="icon-chat"></span> {{ $post->comments_count }} 
-								</a>
+						<div class="text">
+							<h3 class="heading"><a href="{{ route('posts.show', $post) }}"> {{ $post->title }} </a></h3>
+							<p class="excerpt"> {{ $post->excerpt }} </p>
+							<div class="meta">
+								<div><a class='date' href="#"><span class="icon-calendar"></span> {{ $post->created_at->diffForHumans() }} </a></div>
+								<div><a href="#"><span class="icon-user2"></span> {{ $post->author->name }} </a></div>
+								<div class="comments-count">
+									<a href="{{ route('posts.show', $post) }}#post-comments">
+										<span class="icon-chat"></span> {{ $post->comments_count }} 
+									</a>
+								</div>
 							</div>
 						</div>
-					</div>
 					</div>		
 				@empty
 					<p>There are no posts yet.</p>
