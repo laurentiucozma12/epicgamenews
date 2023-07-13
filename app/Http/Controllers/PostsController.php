51fc7124b0bc5662d1029e1b0ee63d6de434d869
Lau <laurentiucozma12@gmail.com>
2023-07-13 13:58:39 +0300

    postsController

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -4,10 +4,24 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Post;
+use App\Models\Category;
+use App\Models\Tag;
 
 class PostsController extends Controller
 {
     public function show(Post $post) {
-        return view('post', ['post' => $post]);
+
+        $recent_posts = Post::latest()->take(5)->get();
+
+        $categories = Category::withCount('posts')->orderBy('posts_count', 'desc')->take(10)->get();
+
+        $tags = Tag::latest()->take(50)->get();
+
+        return view('post', [
+            'post' => $post,
+            'recent_posts' => $recent_posts,
+            'categories' => $categories,
+            'tags' => $tags,
+        ]);
     }
 }
