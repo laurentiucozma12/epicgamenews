f338e197c604d8e7eb71c851b9278784fa86a71f
Lau <laurentiucozma12@gmail.com>
2023-08-31 13:02:45 +0300

    almost deployable

--- app/Services/NavbarServices.php
+++ app/Services/NavbarServices.php
@@ -2,24 +2,31 @@
 
 namespace App\Services;
 
+use Illuminate\Support\Facades\View;
+
+use Illuminate\Support\Facades\Schema;
+
 use App\Models\Category;
 use App\Models\Platform;
 use App\Models\Other;
+
 use Closure;
-use Illuminate\Support\Facades\View;
 
 class NavbarServices
 {
     public function handle($request, Closure $next)
-    {
-        $categories = Category::withCount('posts')->orderBy('posts_count', 'DESC')->take(10)->get();
-        $platforms = Platform::withCount('posts')->orderBy('posts_count', 'DESC')->take(5)->get();
-        $others = Other::withCount('posts')->orderBy('posts_count', 'DESC')->take(5)->get();
-
-        View::share('navbar_categories', $categories);
-        View::share('navbar_platforms', $platforms);
-        View::share('navbar_others', $others);
-
+    {    
+        if (Schema::hasTable('categories') && Schema::hasTable('platforms') && Schema::hasTable('others'))
+        {
+            $categories = Category::withCount('posts')->orderBy('posts_count', 'DESC')->take(10)->get();
+            $platforms = Platform::withCount('posts')->orderBy('posts_count', 'DESC')->take(5)->get();
+            $others = Other::withCount('posts')->orderBy('posts_count', 'DESC')->take(5)->get();
+    
+            View::share('navbar_categories', $categories);
+            View::share('navbar_platforms', $platforms);
+            View::share('navbar_others', $others);
+        }
+    
         return $next($request);
     }
 }
--- app/Services/SettingServices.php
+++ app/Services/SettingServices.php
@@ -5,14 +5,20 @@ namespace App\Services;
 use Closure;
 use Illuminate\Support\Facades\View;
 
+use Illuminate\Support\Facades\Schema;
+
 use App\Models\Setting;
 
 class SettingServices
 {
     public function handle($request, Closure $next)
     {
-        $setting = Setting::find(1);
-        View::share('setting', $setting);
+        
+        if ( Schema::hasTable('settings') )
+        {
+            $setting = Setting::find(1);
+            View::share('setting', $setting);
+        }
 
         return $next($request);
     }
