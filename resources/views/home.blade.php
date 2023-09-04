c120343c6001854d85f36d3336e553c1b2e0a9da
Lau <laurentiucozma12@gmail.com>
2023-09-04 20:40:49 +0300

    test change route

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,7 +14,7 @@
 						<a 
 						href="{{ route('show', $post) }}" 
 						class="blog-img"
-						style="background-image: url({{ asset( 'storage/' . $post->image->path. '' ) }});"></a>
+						style="background-image: url({{ asset( $post->image->path. '' ) }});"></a>
 
 						<div class="text">
 							<h3 class="heading"><a href="{{ route('show', $post) }}"> {{ $post->title }} </a></h3>
