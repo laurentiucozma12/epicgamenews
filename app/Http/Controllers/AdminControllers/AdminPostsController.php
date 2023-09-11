8ef98c65f797354af5a26ff53fa05c6c05e91663
Lau <laurentiucozma12@gmail.com>
2023-09-11 15:48:22 +0300

    testin admin posts controller update method

--- app/Http/Controllers/AdminControllers/AdminPostsController.php
+++ app/Http/Controllers/AdminControllers/AdminPostsController.php
@@ -101,8 +101,8 @@ class AdminPostsController extends Controller
     {
         $this->rules['thumbnail'] = 'nullable|image|dimensions:max_width=1800,max_height=900';
         $validated = $request->validate($this->rules);
-        $validated['approved'] = $request->has('approved');
-    
+        $validated['approved'] = $request->input('approved') !== null;
+
         $post->update($validated);
 
         if ($request->has('thumbnail')) {
