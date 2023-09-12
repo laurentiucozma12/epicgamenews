d3ac80ae5fdab17d2325f14aed34533086c2b7a8
Lau <laurentiucozma12@gmail.com>
2023-09-12 15:39:35 +0300

    testing status approved

--- resources/views/admin_dashboard/posts/index.blade.php
+++ resources/views/admin_dashboard/posts/index.blade.php
@@ -63,10 +63,7 @@
                                     <td>{{ optional($post->other)->name }}</td>                                    
                                     <td>{{ $post->created_at->diffForHumans() }}</td>
                                     <td>
-                                        @php
-                                        //   dd($post->approved);  
-                                        @endphp
-                                        @if($post->approved !== 1)
+                                        @if($post->approved)
                                             <div class="text-info bg-light-info badge rounded-pill p-2 text-uppercase px-3"><i class='bx bxs-circle align-middle me-1'></i>Approved</div>
                                         @else
                                             <div class="text-danger bg-light-danger badge rounded-pill p-2 text-uppercase px-3"><i class='bx bxs-circle align-middle me-1'></i>Not Approved</div>
