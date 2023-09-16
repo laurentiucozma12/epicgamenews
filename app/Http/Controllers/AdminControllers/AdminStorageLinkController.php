65f65287b838cdcb3621e30ef2aea433fd2061f3
Lau <laurentiucozma12@gmail.com>
2023-09-16 18:58:56 +0300

    storage link testing

--- app/Http/Controllers/AdminControllers/AdminStorageLinkController.php
+++ app/Http/Controllers/AdminControllers/AdminStorageLinkController.php
@@ -12,9 +12,8 @@ class AdminStorageLinkController extends Controller
         $branchPaths = [
             'development',
             'pre-production',
-            'production',
             ////// I forgot about this, gotta test if I can delete it storage_path('app/public') //////
-            // 'production' => storage_path('app/public'), // old code
+            'production' => storage_path('app/public'),
         ];
 
         // Get the current branch name (assuming you have Git installed)
@@ -23,29 +22,35 @@ class AdminStorageLinkController extends Controller
         // Check if the current branch exists in the mapping, default to 'production' if not found
         $targetFolder = $branchPaths[$currentBranch] ?? $branchPaths['production'];
 
-        ////// gotta test if I can make it shorter like if ($currentBranch === 'development' || $currentBranch === 'pre-production' || $currentBranch === 'production') {...} //////
+        // NEW Code, I tested it on DEVELOPMENT and it works
+        // I did NOT tested it on PRE-PRODUCTION & PRODUCTION
+        if ($currentBranch === 'development' || $currentBranch === 'pre-production' || $currentBranch === 'production') {
+            $linkFolder = public_path('storage'); 
+            symlink($targetFolder, $linkFolder); }
+
+        // OLD Code
         // Define the target link folder (public storage)
-        if ($currentBranch === 'development')
-        {
-            // dd($targetFolder); // "C:\xampp\htdocs\epicgamenews\storage\app\public"
-            // dd(public_path('storage')); // "C:\xampp\htdocs\epicgamenews\public\storage"
-            $linkFolder = public_path('storage'); 
-            symlink($targetFolder, $linkFolder);        
-        } 
-        else if ($currentBranch === 'pre-production') 
-        {
-            // dd($targetFolder); // "/home/epicjszd/repositories/preprod-epicgamenews/storage/app/public"
-            // dd(public_path('storage')); // "/home/epicjszd/repositories/preprod-epicgamenews/public/storage"
-            $linkFolder = public_path('storage'); 
-            symlink($targetFolder, $linkFolder);
-        } 
-        else if ($currentBranch === 'production') 
-        {        
-            // dd($targetFolder); // "/home/epicjszd/public_html/storage/app/public"
-            // dd(public_path('storage')); // "/home/epicjszd/public_html/public/storage"
-            $linkFolder = public_path('storage'); 
-            symlink($targetFolder, $linkFolder);
-        }
+        // if ($currentBranch === 'development')
+        // {
+        //     // dd($targetFolder); // "C:\xampp\htdocs\epicgamenews\storage\app\public"
+        //     // dd(public_path('storage')); // "C:\xampp\htdocs\epicgamenews\public\storage"
+        //     $linkFolder = public_path('storage'); 
+        //     symlink($targetFolder, $linkFolder);        
+        // } 
+        // else if ($currentBranch === 'pre-production') 
+        // {
+        //     // dd($targetFolder); // "/home/epicjszd/repositories/preprod-epicgamenews/storage/app/public"
+        //     // dd(public_path('storage')); // "/home/epicjszd/repositories/preprod-epicgamenews/public/storage"
+        //     $linkFolder = public_path('storage'); 
+        //     symlink($targetFolder, $linkFolder);
+        // } 
+        // else if ($currentBranch === 'production') 
+        // {        
+        //     // dd($targetFolder); // "/home/epicjszd/public_html/storage/app/public"
+        //     // dd(public_path('storage')); // "/home/epicjszd/public_html/public/storage"
+        //     $linkFolder = public_path('storage'); 
+        //     symlink($targetFolder, $linkFolder);
+        // }
 
         return redirect()->route('home');    
     }
