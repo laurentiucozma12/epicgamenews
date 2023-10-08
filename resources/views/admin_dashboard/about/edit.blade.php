788283882d8ec05868f2ca16975763d1a98b5e60
Lau <laurentiucozma12@gmail.com>
2023-10-08 20:23:30 +0300

    design fixes

--- resources/views/admin_dashboard/about/edit.blade.php
+++ resources/views/admin_dashboard/about/edit.blade.php
@@ -15,9 +15,8 @@
             <div class="ps-3">
                 <nav aria-label="breadcrumb">
                     <ol class="breadcrumb mb-0 p-0">
-                        <li class="breadcrumb-item"><a href="{{ route('admin.index') }}"><i class="bx bx-home-alt"></i></a>
-                        </li>
-                        <li class="breadcrumb-item active" aria-current="page">About Page</li>
+                        <li class="breadcrumb-item"><a href="{{ route('admin.index') }}"><i class="bx bx-home-alt"></i></a></li>                  
+                        <li class="breadcrumb-item active" aria-current="page"><a href="{{ route('admin.about.edit') }}">About Page</a></li>
                     </ol>
                 </nav>
             </div>
