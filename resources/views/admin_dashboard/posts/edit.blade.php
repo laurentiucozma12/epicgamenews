d93e10b2264b4c904930867cfbab3a9704a2a73d
Lau <laurentiucozma12@gmail.com>
2023-10-14 22:11:13 +0300

    testing post edit page

--- resources/views/admin_dashboard/posts/edit.blade.php
+++ resources/views/admin_dashboard/posts/edit.blade.php
@@ -86,11 +86,9 @@
                                                             @foreach ($video_games as $key => $video_game)
                                                             <option {{ $post->video_game_id === $key ? 'selected' : '' }} value="{{ $key }}">{{ $video_game }}</option>
                                                                 {{ 
-                                                                    Log::channel('custom_testing')->info('testing video_game_id input', [
-                                                                        $post->video_game_id,
-                                                                        $key,
-                                                                        $video_game
-                                                                    ]);
+                                                                    Log::channel('custom_testing')->info('testing video_game_id input', $post->video_game_id );
+                                                                    Log::channel('custom_testing')->info('testing video_game_id input', $key]);
+                                                                    Log::channel('custom_testing')->info('testing video_game_id input', $video_game);
                                                                 }}
                                                             @endforeach
                                                         </select>
