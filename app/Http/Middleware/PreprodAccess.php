5987c6dce9c7461a1883511e38c0dc98dbfb48a4
Lau <laurentiucozma12@gmail.com>
2023-10-17 10:49:01 +0300

    ip added

--- app/Http/Middleware/PreprodAccess.php
+++ app/Http/Middleware/PreprodAccess.php
@@ -13,20 +13,20 @@ class PreprodAccess
         $currentBranch = trim(shell_exec('git rev-parse --abbrev-ref HEAD'));
         if ($currentBranch === 'pre-production') 
         {
-            $clientIP = $request->ip();
-            // home ip
-            if ($clientIP === '83.103.225.235'
-            // mobile hotspot
-            || $clientIP === '5.14.142.80'
-            || $clientIP === '5.14.136.80'
-            // localhost
-            || $clientIP === '127.0.0.1')
-            {
+            // $clientIP = $request->ip();
+            // // home ip
+            // if ($clientIP === '83.103.225.235'
+            // // mobile hotspot
+            // || $clientIP === '5.14.142.80'
+            // || $clientIP === '5.14.136.80'
+            // // localhost
+            // || $clientIP === '127.0.0.1')
+            // {
                 return $next($request);
-            }    
-            else {
-                abort(403, 'Access Denied | Unauthorized');
-            }
+            // }    
+            // else {
+            //     abort(403, 'Access Denied | Unauthorized');
+            // }
         }
 
         return $next($request);
