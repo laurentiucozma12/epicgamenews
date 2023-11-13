e7bca3c47b2faf7e6f9fe9de5acb2b3c5fce6a4d
Lau <laurentiucozma12@gmail.com>
2023-11-14 00:23:48 +0200

    add more characters

--- app/Providers/AppServiceProviders.php
+++ app/Providers/AppServiceProviders.php
@@ -28,6 +28,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        Schema::defaultStringLength(191);
         Paginator::useBootstrap();
         if( Schema::hasTable('categories') ) {
             
