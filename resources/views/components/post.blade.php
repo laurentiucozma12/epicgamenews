64c8c6204edc4b2052947da9c79baca4579907a8
Lau <laurentiucozma12@gmail.com>
2023-11-19 22:48:19 +0200

    took of lazy loading

--- resources/views/components/post.blade.php
+++ resources/views/components/post.blade.php
@@ -23,7 +23,7 @@
         <div class="row">
             <div class="col-12">
                 <figure class="animate-box thumbnail-container">			
-                    <img src="{{ $post->image ? asset('storage/' . $post->image->path) : asset('storage/placeholders/thumbnail_placeholder.jpg') }}" width="1800" height="900" alt="{{ $post->image->name }}" title="title {{ $post->image->name }}" loading="lazy"/> 
+                    <img src="{{ $post->image ? asset('storage/' . $post->image->path) : asset('storage/placeholders/thumbnail_placeholder.jpg') }}" width="1800" height="900" alt="{{ $post->image->name }}" title="title {{ $post->image->name }}" /> 
                     @if ($post->author_thumbnail) <figcaption class="author-credit">{{ $post->author_thumbnail }}</figcaption> @endif
                 </figure>
             </div>
