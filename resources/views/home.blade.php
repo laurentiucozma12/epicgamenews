301ed4b099d79e160b8e6c6c97b5171ec7f296ba
Lau <laurentiucozma12@gmail.com>
2023-11-20 03:50:57 +0200

    google ads

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,6 +14,19 @@
 			</div>			
 			<div class="col-12 col-lg-4">
 				
+				<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7545576639006325" crossorigin="anonymous"></script>
+					<!-- Responsive Square Ad -->
+					<ins class="adsbygoogle"
+						style="display:block"
+						data-ad-client="ca-pub-7545576639006325"
+						data-ad-slot="7659623891"
+						data-ad-format="auto"
+						data-full-width-responsive="true">
+					</ins>
+				<script>
+						(adsbygoogle = window.adsbygoogle || []).push({});
+				</script>
+				
 				<x-blog.side-ads/>
 				
 			</div>
