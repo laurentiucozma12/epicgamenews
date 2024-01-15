b57e731756722db4dd44418ed0d0e9706505fdeb
Lau <laurentiucozma12@gmail.com>
2024-01-15 13:24:42 +0200

    scraper puppeteer-extra-plugin-stealth added

--- public/admin_dashboard_assets/js/scraper-puppeteer.js
+++ public/admin_dashboard_assets/js/scraper-puppeteer.js
@@ -10,8 +10,8 @@ const url = 'https://bot.sannysoft.com/';
 async function run() {
     const browser = await puppeteer.launch({ headless: "new", executablePath: executablePath() });
     const page = await browser.newPage();
-    await page.goto(url);  // Use the variable 'url' instead of the string 'url'
-    await page.screenshot({ path: 'public/admin_dashboard_assets/js/bot.png', fullPage: true });  // Corrected method name
+    await page.goto(url);
+    await page.screenshot({ path: 'public/admin_dashboard_assets/js/bot.png', fullPage: true });
 
     // Don't forget to close the browser!
     await browser.close();
