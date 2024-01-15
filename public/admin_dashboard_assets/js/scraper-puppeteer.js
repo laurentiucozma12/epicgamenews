864ecdc5b5408ded34712d5b246390f75d753607
Lau <laurentiucozma12@gmail.com>
2024-01-15 13:16:57 +0200

    scraper puppeteer-extra-plugin-stealth added

--- public/admin_dashboard_assets/js/scraper-puppeteer.js
+++ public/admin_dashboard_assets/js/scraper-puppeteer.js
@@ -1,51 +1,17 @@
-import fs from 'fs';
-import puppeteer from 'puppeteer';
+import puppeteer from 'puppeteer-extra';
+import StealthPlugin from 'puppeteer-extra-plugin-stealth';
+
+puppeteer.use(StealthPlugin());
+
+import { executablePath } from 'puppeteer';
+
+const url = 'https://bot.sannysoft.com/';
 
 async function run() {
-    const browser = await puppeteer.launch({ headless: "new" });
+    const browser = await puppeteer.launch({ headless: "new", executablePath: executablePath() });
     const page = await browser.newPage();
-    await page.goto('https://www.traversymedia.com');
-
-    // await page.screenshot({ path:'public/admin_dashboard_assets/js/scraper-example.png', fullPage: true });
-    // await page.pdf({ path:'public/admin_dashboard_assets/js/scraper-example.pdf', format: 'A4' });
-    
-    // const html = await page.content();
-    // console.log(html);
-
-    // const title = await page.evaluate(() => document.title);
-    // console.log(title);
-
-    // const text = await page.evaluate(() => document.body.innerText);
-    // console.log(text);
-
-    // const links = await page.evaluate(() => 
-    //     Array.from(document.querySelectorAll('a'), (e) => e.href)
-    // );
-    // console.log(links);
-
-    // const courses = await page.evaluate(() => 
-    //     Array.from(document.querySelectorAll('#cscourses .card'), (e) => ({
-    //         title: e.querySelector('.card-body h3').innerText,
-    //         level: e.querySelector('.card-body .level').innerText,
-    //         url: e.querySelector('.card-footer a').href,
-    //     }))
-    // );
-    // console.log(courses);
-
-    const courses = await page.$$eval('#cscourses .card', (elements) => 
-        elements.map(e => ({
-            title: e.querySelector('.card-body h3').innerText,
-            level: e.querySelector('.card-body .level').innerText,
-            url: e.querySelector('.card-footer a').href,
-        }))
-    );
-    console.log(courses);
-
-    // Save data to a json file with indentation
-    fs.writeFile('public/admin_dashboard_assets/js/scraper-example-courses.json', JSON.stringify(courses, null, 2), (err) => {
-        if (err) throw err;
-        console.log('File saved');
-    });
+    await page.goto(url);  // Use the variable 'url' instead of the string 'url'
+    await page.screenshot({ path: 'public/admin_dashboard_assets/js/bot.png', fullPage: true });  // Corrected method name
 
     // Don't forget to close the browser!
     await browser.close();
