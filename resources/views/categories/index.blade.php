d27ad4162d61613fb5f0b74f78744022ff1fcecf
Lau <laurentiucozma12@gmail.com>
2024-03-08 12:31:32 +0200

    design fixes

--- resources/views/categories/index.blade.php
+++ resources/views/categories/index.blade.php
@@ -12,65 +12,47 @@
 		</div>
 	</form>	
 @endsection
-
 <div class="colorlib-blog">
 	<div class="container">
-		<div class="row">
-			<div class="col-md-12 categories-col">
-
-                <div class="row">
-
-                    @forelse ($categories as $category)
-                        <div class='col-md-3'>
-                            <div class="block-21 d-flex animate-box post">
-
-                                <div class="category-container">
-                                    <a href="{{ route('categories.show', $category) }}">
-                                        <div class="image-container">
-                                            <picture>
-                                                <source media="(min-width: 1024px)" sizes="342px" srcset="{{ asset($category->image ? 'storage/images/342x192/' . $category->image->name : 'storage/placeholders/thumbnail_placeholder.jpg') }}">
-                                                <source media="(min-width: 400px)" sizes="764px" srcset="{{ asset($category->image ? 'storage/images/764x431/' . $category->image->name : 'storage/placeholders/thumbnail_placeholder.jpg') }}" loading="{{ ($loop->index > 2) ? 'lazy' : '' }}">
-                                                <source media="(min-width: 300px)" sizes="400px" srcset="{{ asset($category->image ? 'storage/images/400x225/' . $category->image->name : 'storage/placeholders/thumbnail_placeholder.jpg') }}" loading="{{ ($loop->index > 3) ? 'lazy' : '' }}">
-                                                <source media="(min-width: 0px)" sizes="300px" srcset="{{ asset($category->image ? 'storage/images/300x169/' . $category->image->name : 'storage/placeholders/thumbnail_placeholder.jpg') }}" loading="{{ ($loop->index > 4) ? 'lazy' : '' }}">
-                                                @if ($category->image)
-                                                    <img src="{{ asset('storage/images/342x192/' . $category->image->name) }}" alt="{{ $category->image->name }}">
-                                                @else
-                                                    <img src="{{ asset('storage/placeholders/thumbnail_placeholder.jpg') }}" alt="Thumbnail Placeholder">
-                                                @endif
-                                            </picture>
-                                        </div>
-                                        <div class="text-container">
-                                            <h3 class="heading">{{ $category->name }}</h3>                                           
-                                        </div>
-                                    </a>
-                                </div>
-
-                            </div>
-                        </div>
-                    @empty
-                        <p class="lead">There are no categories to show.</p>
-                    @endforelse
-
+        <div class="row">
+            @forelse ($categories as $category)
+                <div class="col-12 col-sm-6 col-md-4 col-lg-3">
+                    <a href="{{ route('categories.show', $category) }}">
+                        <figure class="overlay-container">
+                            <picture>
+                                <source class="image-category" media="(min-width: 1024px)" sizes="342px" srcset="{{ asset($category->image ? 'storage/images/342x192/' . $category->image->name : 'storage/placeholders/thumbnail_placeholder.jpg') }}">
+                                <source class="image-category" media="(min-width: 400px)" sizes="764px" srcset="{{ asset($category->image ? 'storage/images/764x431/' . $category->image->name : 'storage/placeholders/thumbnail_placeholder.jpg') }}" loading="{{ ($loop->index > 2) ? 'lazy' : '' }}">
+                                <source class="image-category" media="(min-width: 300px)" sizes="400px" srcset="{{ asset($category->image ? 'storage/images/400x225/' . $category->image->name : 'storage/placeholders/thumbnail_placeholder.jpg') }}" loading="{{ ($loop->index > 3) ? 'lazy' : '' }}">
+                                <source class="image-category" media="(min-width: 0px)" sizes="300px" srcset="{{ asset($category->image ? 'storage/images/300x169/' . $category->image->name : 'storage/placeholders/thumbnail_placeholder.jpg') }}" loading="{{ ($loop->index > 4) ? 'lazy' : '' }}">
+                                @if ($category->image)
+                                    <img class="image-category" src="{{ asset('storage/images/342x192/' . $category->image->name) }}" alt="{{ $category->image->name }}">
+                                @else
+                                    <img src="{{ asset('storage/placeholders/thumbnail_placeholder.jpg') }}" alt="Thumbnail Placeholder">
+                                @endif
+                            </picture>
+                            <figcaption class="text-container">
+                                <h3 class="heading">{{ $category->name }}</h3>
+                            </figcaption>
+                        </figure>
+                    </a>
                 </div>
-
-                {{ $categories->onEachSide(1)->links('pagination::bootstrap-4') }}
-                
-                <div class="row">
-                    <div class="col-12">  
-                        <div class="d-none d-md-block">
-                            <x-google-ads.responsive-horizontal-ad/>
-                        </div>
-                    </div>
-                    <div class="col-12">
-                        <div class="d-block d-md-none">
-                            <x-google-ads.in-article-ad/>
-                        </div>
-                    </div>
-                </div>
-
-			</div>
-		</div>
-	</div>
+            @empty
+                <p class="lead">There are no Video Games to show.</p>
+            @endforelse   
+        </div>
+    </div>
+    {{ $categories->onEachSide(1)->links('pagination::bootstrap-4') }}
+    <div class="row">
+        <div class="col-12">  
+            <div class="d-none d-md-block">
+                <x-google-ads.responsive-horizontal-ad/>
+            </div>
+        </div>
+        <div class="col-12">
+            <div class="d-block d-md-none">
+                <x-google-ads.in-article-ad/>
+            </div>
+        </div>
+    </div>
 </div>
-
 @endsection
